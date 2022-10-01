ebdc98b701fe36368754e67b3e28dafdd8ceebf1
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:55:25 +0900

    Update Chapter03.java

---
+++ Chapter03.java
@@ -0,0 +1,16 @@
+package javastudy;
+public class Chapter01 {
+    public static void main(String[] args) {
+    int value1 = (3 > 5) ? 6 : 9;
+    System.out.println(value2);
+
+    int value3 = 3;
+    System.out.println((value3 % 2 == 0)) ? "짝수" : "홀수";
+
+    if (value3 % 2 == 0) {
+        System.out.println("짝수");
+    } else {
+        System.out.println("홀수");
+    }
+    }
+} // 9 10 홀수 홀수
