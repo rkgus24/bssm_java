2368ab2f6d14ed8b4e1bcb410239d59da0fe185f
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:55:56 +0900

    Update Chapter05.java

---
+++ Chapter05.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args)
+    { 
+    	 int count;
+         count=0;
+         while(count>0) {
+             System.out.print(count+" ");
+             count++;
+         }
+         count=0;
+         do {
+             System.out.print(count+" ");
+             count++;
+         } while(count<0); 
+    }
+} //0
