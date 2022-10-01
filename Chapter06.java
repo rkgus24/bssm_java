24f1d6ae70b91a4176dc9b0cda230f06c91a37dc
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:56:13 +0900

    Update Chapter06.java

---
+++ Chapter06.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args)
+    { 
+    	 int value1 = 5; 
+         int value2 = 3;
+         if(value1>5) {
+             if(value2<2) {
+                 System.out.println("실행1");
+             }
+             else {
+                 System.out.println("실행2");
+             }
+         }
+         else {
+             System.out.println("실행3");
+         }
+    }
+} //실행3
